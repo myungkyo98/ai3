bf3a339d20e5fa628ad000abbff0ebc5515335d1
myungkyo98 <h098s02612@chunchon.gwe.hs.kr>
2025-11-19 14:21:42 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,17 +77,17 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
 
      labels[1]: {
        "texts": ["흑인", "강함", "킹"],
-       "images": ["https://media.about.nike.com/img/c287f478-579c-4c31-a5da-3a92411694e9/luka-doncic-enlarge2-2.jpg?m=eyJlZGl0cyI6eyJqcGVnIjp7InF1YWxpdHkiOjEwMH0sIndlYnAiOnsicXVhbGl0eSI6MTAwfSwiZXh0cmFjdCI6eyJsZWZ0Ijo3OTQsInRvcCI6MTAsIndpZHRoIjoxMjc1LCJoZWlnaHQiOjIxMjN9LCJyZXNpemUiOnsid2lkdGgiOjM4NDB9fX0%3D&s=383fe9bab9113f62527527c9c79a8719d45edbdf7d0213113a9373d21d927848"],
+       "images": ["https://search.pstatic.net/common/?src=http%3A%2F%2Fimgnews.naver.net%2Fimage%2F5838%2F2025%2F10%2F22%2F0000019417_001_20251022184214828.jpg&type=sc960_832"],
        "videos": ["https://www.youtube.com/watch?v=-9lP95Qo-I0"]
      },
     labels[2]: {
        "texts": ["한국인", "수비", "놔둬"],
-       "images": ["https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.hani.co.kr%2Farti%2Fsports%2Fsports_general%2F925878.html&psig=AOvVaw3Ee8J-Hq_WLRq1rUAzW28G&ust=1763522360139000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCMC_z_He-pADFQAAAAAdAAAAABAE"],
+       "images": ["https://search.pstatic.net/common/?src=http%3A%2F%2Fimgnews.naver.net%2Fimage%2F028%2F2020%2F01%2F28%2F0002482953_003_20200129024603197.jpg&type=sc960_832"],
        "videos": ["https://www.youtube.com/watch?v=X3lVsJdV05w"]
     },
     labels[3]: {
-       "texts": ["한국인", "수비", "놔둬"],
-       "images": ["https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.rookie.co.kr%2Fnews%2FarticleView.html%3Fidxno%3D53670&psig=AOvVaw1wv0C2N6ZjFmd_355gUJn3&ust=1763522450186000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCIjwqp_f-pADFQAAAAAdAAAAABAJ"],
+       "texts": ["흑인", "길쭉", "미들"],
+       "images": ["https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20150709_126%2Fpuremind28_1436430846347ld3z2_JPEG%2FKD2.jpg&type=sc960_832"],
        "videos": ["https://www.youtube.com/watch?v=pE_KNAw5pdY"]
     }
     
